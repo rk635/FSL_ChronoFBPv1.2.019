25f3e33b64819eb400f58eb9f70d4b9f8673c579
Sean Kannanaikal <Sean_Kannanaikal@Seans-MBP.fios-router.home>
2019-08-01 18:10:40 -0400

    Playing Sound

--- script.py
+++ script.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
+from pygame import mixer, time
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello World!"
+        return "Start Page"
 
 @app.route('/examination')
 def examination():
-    return "Examination Screen"
-    
+ mixer.init()
+ mixer.music.load('sond.mp3')
+ mixer.music.play()
+ while mixer.music.get_busy():
+         time.Clock().tick(10)
+ return "Examination Screen"
+
+
+
 @app.route('/results')
 def results():
     return "Results Screen"
