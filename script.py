b8e9a4c77013c35fd127cac7255957dd01e06a99
Sean Kannanaikal <Sean_Kannanaikal@Seans-MBP.fios-router.home>
2019-08-02 20:16:40 -0400

    Monitor Function added

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 from playsound import playsound
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Listener
 import random 
+import keyboard
 import time as t
 
 app = Flask(__name__, template_folder='templates')
@@ -17,6 +18,11 @@ def examination():
         playsound('audio_files/' + str(song_number) + '.mp3')
         #start_time = t.time()
         
+        def key_press(key):
+                print(key.name)
+                keyboard.on_press(key_press)
+        while True:
+                t.sleep(1)
         return "examination screen"
 
 
